52b0314033307f3e7943254b70e48c37dbfb47f7
Kiruha <kirillkorzunov9000@gmail.com>
2023-12-26 22:58:22 +0300

    check the work

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.getAllUsers();
+
+        userService.saveUser("Ivan", "Ivanov", (byte) 14);
+        userService.saveUser("Petr", "Petrov", (byte) 45);
+        userService.saveUser("Semen", "Semenov", (byte) 27);
+        userService.saveUser("Bob", "I", (byte) 3);
+        userService.getAllUsers();
+
+        userService.removeUserById(2);
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+        userService.getAllUsers();
+
+        userService.dropUsersTable();
     }
 }
